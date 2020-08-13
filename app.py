af243dba12f68eaa474f7f3497ddd9b4a7b3050e
Gloria Vela <gvela@def-logix.com>
2020-08-13 08:51:22 -0500

    Use the code snippet to add pages

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask"
-
+    return render_template("home.html")
 
 @app.route("/hello/<name>")
 def hello_there(name = None):
@@ -26,3 +25,15 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+# Replace the existing home function with the one below
+
+
+# New functions
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
